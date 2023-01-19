2ffd81b5ca0101b2605eb0f42e80468899b40642
OHovey <30669691+OHovey@users.noreply.github.com>
2023-01-19 18:10:53 +0000

    latest

--- netlify/functions/update-and-build-background.ts
+++ netlify/functions/update-and-build-background.ts
@@ -150,7 +150,7 @@ exports.handler = async (event) => {
     // declare some details for the markdown
     let d = new Date();
     let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
-    let mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d);
+    let mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(d);
     let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
     const timestamp = `${da}-${mo}-${ye}`;
     const author = "roboman";
