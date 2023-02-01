1425573206d16ad8289666492a356985a51d6712
OHovey <30669691+OHovey@users.noreply.github.com>
2023-02-01 10:39:18 +0000

    updated regulated background tasks

--- netlify/functions/query-keywords.ts
+++ netlify/functions/query-keywords.ts
@@ -5,20 +5,11 @@ import axios from 'axios';
 const myHandler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
     console.log("Received event:");
 
-    const result = await axios.get('https://api.apify.com/v2/actor-tasks/VncdzeYjbYNubPpkY/run-sync?token=apify_api_FpKFZk1nVdehSrF7HedZ8x7Cqxdnzp0T4bhK', {
+    axios.post('https://sprightly-bonbon-6b5065.netlify.app/.netlify/functions/update-and-build-background', {}, {
         headers: {
-            "Content-Type": "application/json"
+            'Authorization': `Bearer ${process.env.PERSONAL_ACCESS_TOKEN}`
         }
-    }).then( _ => {
-
-        axios.post('https://sprightly-bonbon-6b5065.netlify.app/.netlify/functions/update-and-build-background', {}, {
-            headers: {
-                'Authorization': `Bearer ${process.env.PERSONAL_ACCESS_TOKEN}`
-            }
-        });
-    }).catch(err => {
-        console.log("error: " + err)
-    })
+    });
 
     return {
         statusCode: 200,
--- netlify/functions/update-and-build-background.ts
+++ netlify/functions/update-and-build-background.ts
@@ -89,162 +89,171 @@ exports.handler = async (event) => {
 
     console.log("Received event:");
 
-    const result = await axios.get(`https://api.apify.com/v2/actor-tasks/VncdzeYjbYNubPpkY/runs/last/dataset/items?token=${process.env.APIFY_TOKEN}&status=SUCCEEDED`, {
+
+    await axios.get('https://api.apify.com/v2/actor-tasks/VncdzeYjbYNubPpkY/run-sync?token=apify_api_FpKFZk1nVdehSrF7HedZ8x7Cqxdnzp0T4bhK', {
         headers: {
             "Content-Type": "application/json"
         }
-    }).catch(err => {
-        console.log("error: " + err)
-    })
-
-    
-    const { data: { login } } = await octokit.rest.users.getAuthenticated();
-
-    let articles = {};
-
-    // @ts-ignore
-    const querys: Array<string> = [...new Set(result.data.filter( query => query.hasOwnProperty("parentQuery") ).map( query => query.parentQuery))];
+    }).then( async _ => {
+        
     
 
-    // Promise.resolve( async () => {
+        const result = await axios.get(`https://api.apify.com/v2/actor-tasks/VncdzeYjbYNubPpkY/runs/last/dataset/items?token=${process.env.APIFY_TOKEN}&status=SUCCEEDED`, {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).catch(err => {
+            console.log("error: " + err)
+        })
 
-    //     let queryIndex = 0;
+        
+        const { data: { login } } = await octokit.rest.users.getAuthenticated();
 
-    //     const createArticle = async (term) => {
+        let articles = {};
 
-    //         return await ( async () => {
-    //             if (queryIndex >= querys.length) return false;
-    //             let headline: string = (await fetchHeadline(term)).replace("\n\n", "").replace(":", " -"); 
+        // @ts-ignore
+        const querys: Array<string> = [...new Set(result.data.filter( query => query.hasOwnProperty("parentQuery") ).map( query => query.parentQuery))];
+        
 
-    //             console.log('headline: ' + headline)
+        // Promise.resolve( async () => {
 
-    //             let article = await fetchArticleContent(term, headline);
+        //     let queryIndex = 0;
 
-    //             // declare some details for the markdown
-    //             const timestamp = new Date().toUTCString();
-    //             const author = "roboman";
-    //             const metaData = `---\ntitle: ${headline}\nauthor: ${author}\ndate: ${timestamp}\n---\n`;
+        //     const createArticle = async (term) => {
 
-    //             article = `${metaData}${article}`
+        //         return await ( async () => {
+        //             if (queryIndex >= querys.length) return false;
+        //             let headline: string = (await fetchHeadline(term)).replace("\n\n", "").replace(":", " -"); 
 
-    //             articles[headline] = article;
+        //             console.log('headline: ' + headline)
+
+        //             let article = await fetchArticleContent(term, headline);
+
+        //             // declare some details for the markdown
+        //             const timestamp = new Date().toUTCString();
+        //             const author = "roboman";
+        //             const metaData = `---\ntitle: ${headline}\nauthor: ${author}\ndate: ${timestamp}\n---\n`;
+
+        //             article = `${metaData}${article}`
+
+        //             articles[headline] = article;
+                    
+        //             queryIndex += 1;
+        //         })().then(res => {
+        //             if (res != false) {
+        //                 createArticle(querys[queryIndex]);
+        //             } else {
+        //                 return;
+        //             }
+        //         })
+        //     }
+
+        //     return await createArticle(querys[queryIndex]);
+        // }).then( _ => {
+        //     Object.keys(articles).map( async articleTitle => {
+
+        //         const title = articleTitle.split(' ').join('-');
+
+        //         await octokit.request(`PUT /repos/OHovey/Neutrality-Media/contents/src/data/articles/${title}.md`, {
+        //             owner: 'OHovey',
+        //             repo: 'Neutrality-Media',
+        //             path: `/src/data/articles/${title}.md`,
+        //             message: `created new article: ${title} [skip ci]`,
+        //             committer: {
+        //                 name: 'Oliver Hovey',
+        //                 email: 'olliehovey@gmail.com'
+        //             },
+        //             content: Buffer.from(articles[articleTitle]).toString('base64')
+        //         }).then( res => {
+
+        //             console.log(res);
+        //         }).catch( err => {
+
+        //             console.error(err);
+        //         })
+        //     })
+
+        //     // rebuild the site via netlify api
+        //     axios.post(`https://api.netlify.com/api/v1/oauth/tickets`, {
+        //         client_id: process.env.NETLIFY_CLIENT_ID
+        //     }).then( ticket => {
                 
-    //             queryIndex += 1;
-    //         })().then(res => {
-    //             if (res != false) {
-    //                 createArticle(querys[queryIndex]);
-    //             } else {
-    //                 return;
-    //             }
-    //         })
-    //     }
+        //         axios.post(`https://api.netlify.com/api/v1/sites/${process.env.NETLIFY_SITE_ID}/deploys`, {
+        //         files: {},
+        //         draft: false,
+        //         async: true,
+        //         functions: {},
+        //         functions_scedules: [],
+        //         branch: "main",
+        //         framework: "gatsby"
+        //     })
+        //     })
+        // })
 
-    //     return await createArticle(querys[queryIndex]);
-    // }).then( _ => {
-    //     Object.keys(articles).map( async articleTitle => {
+        const term: string = querys[0];
 
-    //         const title = articleTitle.split(' ').join('-');
+        let headline: string = (await fetchHeadline(term)).replace("\n\n", "").replace(":", " -"); 
 
-    //         await octokit.request(`PUT /repos/OHovey/Neutrality-Media/contents/src/data/articles/${title}.md`, {
-    //             owner: 'OHovey',
-    //             repo: 'Neutrality-Media',
-    //             path: `/src/data/articles/${title}.md`,
-    //             message: `created new article: ${title} [skip ci]`,
-    //             committer: {
-    //                 name: 'Oliver Hovey',
-    //                 email: 'olliehovey@gmail.com'
-    //             },
-    //             content: Buffer.from(articles[articleTitle]).toString('base64')
-    //         }).then( res => {
+        let article = await fetchArticleContent(term, headline);
 
-    //             console.log(res);
-    //         }).catch( err => {
+        // generate image
+        const imageData = await axios.post('https://api.openai.com/v1/images/generations', {
+            prompt: `An image to caption the following headline: ${headline}`,
+            n: 1,
+            size: "1024x1024"
+        },{
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${process.env.OPENAI_AUTH_TOKEN}`
+            }
+        }).catch( err => {
+            console.error(err);
+        })
 
-    //             console.error(err);
-    //         })
-    //     })
+        // console.log(imageData.data.data.url)
 
-    //     // rebuild the site via netlify api
-    //     axios.post(`https://api.netlify.com/api/v1/oauth/tickets`, {
-    //         client_id: process.env.NETLIFY_CLIENT_ID
-    //     }).then( ticket => {
+        const imageUrl = imageData.data.data[0].url;
+
+        // download The image
+        await downloadImage(imageUrl, headline.split(' ').join('-'));
+
+        console.log("imageUrl: " + imageUrl)
+
+        // declare some details for the markdown
+        let d = new Date();
+        let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+        let mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(d);
+        let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+        const timestamp = `${ye}-${mo}-${da}`;
+        const author = "roboman";
+        const metaData = `---\ntitle: ${headline}\nauthor: ${author}\ndate: ${timestamp}\nimageUrl: ../images/${headline.split(' ').join('')}\narticlesImageUrl: ../../images/${headline.split(' ').join('')}\n---\n`;
+
+        article = `${metaData}${article}`
+
+        articles[headline] = article;
+
+        const title = headline.split(' ').join('-');
+
+
+        await octokit.request(`PUT /repos/OHovey/Neutrality-Media/contents/src/data/articles/${title}.md`, { 
+            owner: 'OHovey',
+            repo: 'Neutrality-Media',
+            path: `/src/data/articles/${title}.md`,
+            message: `created new article: ${title}`, // if uploading multiple files again... upload commit message with "[skip ci]" for all but last.
+            committer: {
+                name: 'Oliver Hovey',
+                email: 'olliehovey@gmail.com'
+            },
+            content: Buffer.from(articles[headline]).toString('base64')
+        }).then( res => {
+
+            // console.log(res);
+        }).catch( err => {
+
+            console.error(err);
+        })
             
-    //         axios.post(`https://api.netlify.com/api/v1/sites/${process.env.NETLIFY_SITE_ID}/deploys`, {
-    //         files: {},
-    //         draft: false,
-    //         async: true,
-    //         functions: {},
-    //         functions_scedules: [],
-    //         branch: "main",
-    //         framework: "gatsby"
-    //     })
-    //     })
-    // })
-
-    const term: string = querys[0];
-
-    let headline: string = (await fetchHeadline(term)).replace("\n\n", "").replace(":", " -"); 
-
-    let article = await fetchArticleContent(term, headline);
-
-    // generate image
-    const imageData = await axios.post('https://api.openai.com/v1/images/generations', {
-        prompt: `An image to caption the following headline: ${headline}`,
-        n: 1,
-        size: "1024x1024"
-    },{
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${process.env.OPENAI_AUTH_TOKEN}`
-        }
-    }).catch( err => {
-        console.error(err);
     })
-
-    // console.log(imageData.data.data.url)
-
-    const imageUrl = imageData.data.data[0].url;
-
-    // download The image
-    await downloadImage(imageUrl, headline.split(' ').join('-'));
-
-    console.log("imageUrl: " + imageUrl)
-
-    // declare some details for the markdown
-    let d = new Date();
-    let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
-    let mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(d);
-    let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
-    const timestamp = `${ye}-${mo}-${da}`;
-    const author = "roboman";
-    const metaData = `---\ntitle: ${headline}\nauthor: ${author}\ndate: ${timestamp}\nimageUrl: ../images/${headline.split(' ').join('')}\narticlesImageUrl: ../../images/${headline.split(' ').join('')}\n---\n`;
-
-    article = `${metaData}${article}`
-
-    articles[headline] = article;
-
-    const title = headline.split(' ').join('-');
-
-
-    await octokit.request(`PUT /repos/OHovey/Neutrality-Media/contents/src/data/articles/${title}.md`, { 
-        owner: 'OHovey',
-        repo: 'Neutrality-Media',
-        path: `/src/data/articles/${title}.md`,
-        message: `created new article: ${title}`, // if uploading multiple files again... upload commit message with "[skip ci]" for all but last.
-        committer: {
-            name: 'Oliver Hovey',
-            email: 'olliehovey@gmail.com'
-        },
-        content: Buffer.from(articles[headline]).toString('base64')
-    }).then( res => {
-
-        // console.log(res);
-    }).catch( err => {
-
-        console.error(err);
-    })
-            
-
     // (async () => {
 
     //     const term: string = querys[0];
